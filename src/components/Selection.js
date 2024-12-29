4ea4406f7195aa41745216a9039f027a081e3005
Tuhin1904 <iamtuhinn@gmail.com>
2024-12-29 23:20:25 +0530

    box-color

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
+import React, { useState } from 'react'
+import './../styles/Child.css'
 
+const Selection = ( {applyColor}) => {
+    const [background, setBackground] = useState({ backgroundColor: '' });
+    const updateSelectionStyle = (newBackground) => {
+        console.log(newBackground)
+        setBackground(newBackground);
+    };
+
+  return (
+    <div className='fix-box' style={background} onClick={() => applyColor(updateSelectionStyle)}>
+        <p className='subheading'>Selection</p>
+    </div>
+  )
+}
+
+export default Selection
